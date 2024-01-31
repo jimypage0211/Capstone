8c0b034598280831699c7da9ba829d509203b562
jimypage0211 <jimypage@bu.edu>
2024-01-31 14:03:39 -0500

    Altered models

--- reservations/admin.py
+++ reservations/admin.py
@@ -4,3 +4,4 @@ from .models import *
 admin.site.register(User)
 admin.site.register(Restaurant)
 admin.site.register(Reservation)
+admin.site.register(Shift)
--- reservations/models.py
+++ reservations/models.py
@@ -4,48 +4,92 @@ from django.db import models
 
 class User(AbstractUser):    
     pass
-
-
+        
+shiftsArray = [
+    "10AM - 12PM",
+    "12PM - 2PM",
+    "2PM - 4PM",
+    "4PM - 6PM",
+    "6PM - 8PM",
+    "8PM - 10PM",
+    "10PM - 12AM"
+]
+        
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     personCapacity = models.IntegerField(null = False)
     tablesCapacity = models.IntegerField(null = False)
-    reserverdTables = models.IntegerField()
-    isFull = models.BooleanField(default = False)
 
     def __str__(self):
-        return self.name
+        return self.name   
+    
+    def save (self, *args, **kwargs):
+        super(Restaurant,self).save(*args, **kwargs)
+        for shift in shiftsArray:
+            shiftToSave = Shift(
+                shiftRange = shift,
+                restaurant = self
+            )
+            shiftToSave.save()
+
     
     def serialize (self):
-        reservations = self.reservations.all()
-        reservationsIDs = []
-        for reservation in reservations:
-            reservationsIDs.append(reservation.id)
+        shifts = self.shifts.all()
+        shiftsIDS = []
+        for shift in shifts:
+            shiftsIDS.append(shift.id)
         return {
             "name": self.name,
             "personCapacity": self.personCapacity,
-            "maxTables": self.tablesCapacity,
-            "reserverdTables": self.reserverdTables,
+            "tablesCapacity": self.tablesCapacity,
             "isFull": self.isFull,
-            "reservationsIDs": reservationsIDs
+            "shiftsIDS": shiftsIDS
         }
+        
+        
+class Shift (models.Model):    
+    shiftRange = models.CharField(max_length=11)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name= "shifts")   
+    personCapacity = models.IntegerField(default = -1)
+    tablesCapacity = models.IntegerField(default = -1)
+    isFull = models.BooleanField(default = False, editable = False)
+    
+    
+    def __str__(self):
+        return f"{self.restaurant} - {self.shiftRange}"
+    
+    def save (self, *args, **kwargs):
+        if self.personCapacity == -1:
+            self.personCapacity = self.restaurant.personCapacity
+            self.tablesCapacity = self.restaurant.tablesCapacity    
+        
+        super(Shift,self).save(*args, **kwargs)
 
 class Reservation(models.Model):
-    time  = models.DateTimeField()
     client = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "reservations")
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name = "reservations")
     numberOfDiners = models.IntegerField()
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE, related_name = "reservations")
+    
+    def save (self, *args, **kwargs):
+        shiftToReserve = Shift.objects.get(id = self.shift.id)
+        shiftToReserve.tablesCapacity = shiftToReserve.tablesCapacity -1
+        shiftToReserve.personCapacity = shiftToReserve.personCapacity - self.numberOfDiners
+        if shiftToReserve.tablesCapacity == 0:
+            shiftToReserve.isFull = True
+        shiftToReserve.save()
+        super(Reservation,self).save(*args, **kwargs)        
+        
 
     def __str__(self):
-        return f"{self.client.email} - {self.time} - {self.restaurant.name}"
+        return f"{self.client.email} - {self.restaurant.name}"
 
     def serialize (self):
         return {
-            "time": self.time,
             "client": self.client,
             "restaurant": self.restaurant,
-            "numberOfDiners": self.numberOfDiners
+            "numberOfDiners": self.numberOfDiners,
+            "shift": self.shift.shiftRange
         }
-
 
 
